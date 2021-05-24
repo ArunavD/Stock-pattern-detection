a2a55d1e31f144bec02861bf8c0d70f30c2e9f0a
ArunavD <arunavdas15@gmail.com>
2021-05-24 23:41:38 +0530

    visualization

---
+++ tesla_pattern.py
@@ -0,0 +1,10 @@
+import csv
+
+with open("tesla.csv") as f:
+    reader = csv.DictReader(f)
+    #print(reader)
+    candles = list(reader)
+
+
+for i in range(1, len(candles)):
+    print(candles[i])
---
+++ tesla_price_visualisation.py
@@ -0,0 +1,45 @@
+
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import mplfinance as mpf
+
+
+file = 'tesla.csv'
+data = pd.read_csv(file)
+#print(data)
+#data.info()
+
+
+data.Date = pd.to_datetime(data.Date)
+#data.info()
+
+
+data = data.set_index('Date')
+#print(data)
+
+#Plotting the data
+#mpf.plot(data)
+
+#line and vloume type plot
+#mpf.plot(data,type = 'line', volume = True)
+
+#candle stick plot
+#mpf.plot(data,type ='candle', volume = True)
+
+
+#candle stick chart of a specific month
+#mpf.plot(data['2020-07'],type ='candle', volume = True)
+
+
+#candle stick chart for a time frame
+#mpf.plot(data['2020-07':'2020-08'],type ='candle',volume = True, tight_layout = True)
+
+
+#Adding title and style
+mpf.plot(data['2020-07':'2020-08'],
+         type = 'candle',
+         title = 'Tesla price 2020\21',
+         volume = True, 
+         tight_layout = True,
+         style = 'yahoo')
