19315833a664fb9460df73255ec4b02ba10ec2f9
ArunavD <arunavdas15@gmail.com>
2021-05-25 22:00:58 +0530

    bulish engulfing detection for "TSLA"

--- tesla_pattern.py
+++ tesla_pattern.py
@@ -1,6 +1,24 @@
 import csv
 
 
+def is_barish_candlestick(candles):
+    return candles["Close"] < candles["Open"]
+
+
+def is_bullish_engulfing(candles,index):
+    current_day = candles[index]
+    previous_day = candles[index-1]
+
+    if is_barish_candlestick(previous_day)\
+        and current_day["Open"] < previous_day["Close"]\
+        and current_day["Close"] > previous_day["Open"]:
+        return True
+    
+    return False
+
+
+
+
 with open("tesla.csv") as f:
     reader = csv.DictReader(f)
     #print(reader)
@@ -8,4 +26,11 @@ with open("tesla.csv") as f:
 
 
 for i in range(1, len(candles)):
-    print(candles[i])
+    #print(candles[i])
+
+    if is_bullish_engulfing(candles,i):
+        print("{} It is bulish engulfing".format(candles[i]['Date']))
+
+
+
+
